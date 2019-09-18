546392645188fb2955c6bd69df0349346fe1faf4
thomas.hui.yang <17292795+yuezhilanyi@users.noreply.github.com>
2019-09-18 19:19:34 +0800

    temp commit to transfer codes

--- reg2.py
+++ reg2.py
@@ -25,6 +25,8 @@ def read_source(file_path, sheetname=0):
 
     # filter with ["问题类型"] == "事件"
     df = df[df["问题类型"] == "事件"]
+    # filter with ['当前阶段'] == '[作废]'
+    df = df[df['当前阶段'] != '[作废]']  # 根据2019.9.18与网格中心考评处的沟通, 不考虑作废案件
 
     # keep only useful columns, to reduce too many dimensions
     df = df[["问题类型", "街道", "上报时间", "当前阶段", "处置时限", "处置用时", "结案时间", "处置超时倍数",
@@ -43,10 +45,18 @@ def read_source(file_path, sheetname=0):
 def convert_to_new_dataframe(df):
     # # add some fields
     df["结案日期"] = df["结案时间"].dt.date
+    df_gt = pd.read_excel("../ZS222.xlsx")
+    df_gt.set_index(df_gt["日期"], inplace=True)
 
     # new dataframe
     index = df["结案日期"].value_counts().index
     index = sorted(index)
+
+    def get_gt(dataframe_gt, somewhere, someday):
+        iso_someday = int(someday.isoformat().replace('-', ''))
+        dataframe_gt = dataframe_gt.xs(iso_someday)
+        gt = dataframe_gt[somewhere]
+        return gt
 
     def dataframe2list(dataframe, somewhere, someday):
         """
@@ -56,22 +66,26 @@ def convert_to_new_dataframe(df):
         :param someday: datetime.date
         :return: list consist of str, float and int values
         """
+
         dataframe = dataframe[dataframe["街道"] == somewhere]
         dataframe1 = dataframe[dataframe["结案日期"] == someday]
         # TODO: 添加未结案的并加权
+        # 截至表格导出日, 未结案
         dataframe2 = dataframe[pd.isnull(dataframe["结案日期"])]
         dataframe2 = dataframe2[dataframe2["上报时间"] <= someday]
+        # 截至结案日当天, 未结案
+        dataframe3 = dataframe[dataframe["上报时间"] < someday]
+        dataframe3 = dataframe3[dataframe3["结案日期"] > someday]
 
-        merge = dataframe1.append(dataframe2)
+        merge = dataframe1.append(dataframe2).append(dataframe3)
 
         total = len(merge)  # 案件总数
         dti = 0  # disposal time index
         t7 = len(dataframe2)  # "未结案总数"
-        t8 = len(merge[merge['当前阶段'] == '[作废]'])  # "作废案件总数"
-        t1, t2, t3, t4, t5, t6, = 0, 0, 0, 0, 0, 0  # "自处置总数", "强制结案总数", "实际办结总数",
+        t1, t2, t3, t4, t5, t6 = 0, 0, 0, 0, 0, 0  # "自处置总数", "强制结案总数", "实际办结总数",
         # "准办结总数", "返工总数", "超时结案总数"
+        t8 = get_gt(df_gt, somewhere, someday)
         for _, row in merge.iterrows():
-            total += 1
             if pd.isnull(row["处置时限"]):  # 某些没有处置时限, 比如作废的
                 dti += 0
             else:
@@ -95,20 +109,27 @@ def convert_to_new_dataframe(df):
             t4 += row["准办结数"]
             t5 += row["返工数"]
             t6 += row["超时结案数"]
-        return someday, somewhere, total, dti / total, t1, t2, t3, t4, t5, t6, t7, t8
+        if total != 0:
+            dti = dti / total
+        else:
+            dti = -1
+        return someday, somewhere, total, dti, t1, t2, t3, t4, t5, t6, t7, t8
 
     lst = []
-    for i in tqdm.trange(len(index)):
+    # for i in tqdm.trange(len(index)):
+    for i in tqdm.trange(40):
         day = index[i]
         assert day is not None
-        for area in df["街道"].value_counts().index:
+        # for area in df["街道"].value_counts().index:
+        for area in ["东华门", "景山", "交道口", "安定门", "北新桥", "东四", "朝阳门", "建国门", "东直门", "和平里",
+                     "前门", "崇外", "东花市", "龙潭", "体育馆", "天坛", "永定门外"]:  # 和网格代码顺序一致, 方便后续观察对比
             # tqdm.tqdm.write(area)
             result = dataframe2list(df, area, day)
             lst.append(result)
 
     res = pd.DataFrame(lst, columns=["日期", "街道",
                                      "案件总数", "处置及时指数", "自处置总数", "强制结案总数", "实际办结总数",
-                                     "准办结总数", "返工总数", "超时结案总数", "未结案总数", "作废案件总数"])
+                                     "准办结总数", "返工总数", "超时结案总数", "未结案总数", 'GT'])
 
     return res
 
@@ -166,7 +187,7 @@ if __name__ == "__main__":
     # df1.to_pickle('../event_data_simplified.npy')
 
     df2 = convert_to_new_dataframe(df1)
-    df2.to_csv('../ndf.csv')
+    df2.to_excel('../ndf.xlsx')
 
     # dm = DataModel(df1)
     # df1 = dm.cal_all()
--- reg3.py
+++ reg3.py
@@ -1,34 +1,157 @@
-import scipy.stats
+import os
+import tqdm
+
 import pandas as pd
-
-from collections import OrderedDict
-
-from reg1 import df2a, df2b
-from reg1 import linear_reg_test, polynomial_reg_test, convert_data_and_label_to_train
-from reg2 import read_excel2
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from datetime import datetime, timedelta
 
 
-def cal_coef(train_df, label_df):
-    SPM = scipy.stats.spearmanr
-    res = OrderedDict()
-    for col in train_df.columns:
-        coef, p = (SPM(train_df[col], label_df))
-        res[col] = (coef, p)
-    return res
+def read_source(file_path, sheetname=0):
+    """
+    读取源文件并简化，最后以结案日期为索引，生成一张新表用作训练和测试
+    :param file_path:
+    :param sheetname:
+    :return:
+    """
+    # read into dataframe
+    if file_path.endswith("npy"):
+        df = pd.read_pickle(file_path)
+    elif file_path.endswith("xlsx") or file_path.endswith("xls"):
+        df = pd.read_excel(file_path, sheetname=sheetname)
+        # df.to_pickle('../event_data.npy')
+    else:
+        raise Exception("Only 'npy' or 'xlsx' or 'xls' supported, input file type: {}".format(file_path.split('.'[-1])))
+
+    # filter with ["问题类型"] == "事件"
+    df = df[df["问题类型"] == "事件"]
+    # filter with ['当前阶段'] == '[作废]'
+    df = df[df['当前阶段'] != '[作废]']  # 根据2019.9.18与网格中心考评处的沟通, 不考虑作废案件
+
+    # keep only useful columns, to reduce too many dimensions
+    df = df[['案件号', '问题来源', '问题类型', '大类名称', '街道', '上报时间', '当前阶段', '处置截止时间','处置结束时间',
+             '结案时间', '立案时间', '强制结案时间']]
+    """
+    '案件号', '问题来源', '问题类型', '大类名称', '街道', '上报时间', '当前阶段', '延期类型', '案件标识', '处置截止时间', 
+    '处置结束时间', '强结限制', '案件类型', '结案时间', '首次派遣时间', '受理开始时间', '案件阶段名称', '受理时间', 
+    '立案时间', '强制结案时间'
+    """
+    return df
 
 
-work_excel = 'work2.xlsx'
-# read excel and join multiple rows into one row
-df1 = read_excel2(work_excel)
-# del df1['x7'], df1['x8']
+def convert_to_new_dataframe(df):
+    # # add some fields
+    df["结案日期"] = df["结案时间"].dt.date
+    df_gt = pd.read_excel("../ZS222.xlsx")
+    df_gt.set_index(df_gt["日期"], inplace=True)
+
+    # new dataframe
+    index = df["结案日期"].value_counts().index
+    index = sorted(index)
+
+    def get_gt(dataframe_gt, somewhere, someday):
+        iso_someday = int(someday.isoformat().replace('-', ''))
+        dataframe_gt = dataframe_gt.xs(iso_someday)
+        gt = dataframe_gt[somewhere]
+        return gt
+
+    def dataframe2list(dataframe, somewhere, someday):
+        """
+
+        :param dataframe: pandas dataframe
+        :param somewhere: str,
+        :param someday: datetime.date
+        :return: list consist of str, float and int values
+        """
+        # F1 - 某地所有案件
+        dataframe = dataframe[dataframe["街道"] == somewhere]
+
+        # F2.1 - 监督员自行处理的案件
+        df_self = dataframe[dataframe["问题来源"] == "监督员自行处理"]
+        df_self = df_self[dataframe["上报时间"].dt.date == someday]  # 某日自行处理案件, 默认为当天完成(结案时间不正确)
+        n1 = len(df_self)  # 自行处理案件总数
+        # F2.2 - 剩余案件 (均具有处置截止时间, 处置结束时间, 结案时间, 立案时间等信息)
+        dataframe = dataframe[dataframe["问题来源"] != "监督员自行处理"]
+
+        # F3 (<-F2.2) - 考核日之前的所有案件
+        dataframe = dataframe[dataframe["上报时间"] <= someday]
+        # F3.1 非考核日之前结案
+        dataframe = dataframe[-dataframe["结案日期"] < someday]
+
+        n2 = len(dataframe)  # 非自行处理案件总数
+        dti = 0  # disposal time index
+
+        t1, t2p, t2a = None, None, None
+
+        for _, row in dataframe.iterrows():
+            t1 = (row["立案时间"] - row["上报时间"]).seconds
+            t2p = (row["处置截止时间"] - row["立案时间"]).seconds
+            t2a = (row["处置结束时间"] - row["立案时间"]).seconds
+        return someday, somewhere, n1, n2, t1, t2p, t2a
+
+    df["立案耗时"] = df["立案时间"].subtract(df["上报时间"])
+    df["处置预计耗时"] = df["处置截止时间"].subtract(df["立案时间"])
+    df["处置实际耗时"] = df["处置结束时间"].subtract(df["立案时间"])
+
+    return df
+
+
+def convert_data_and_label_to_train(data_df, label_df):
+    df = pd.merge(data_df, label_df, on='area')
+    train_label = df['mean_value']
+    train_data = df.drop('mean_value', 1)
+    del train_data['area']
+    return train_data, train_label
+
+
+def linear_reg_test(train_data, train_label, test_data, index):
+    """
+    线性回归
+    """
+    lr = LinearRegression(normalize=True)
+    lr.fit(train_data, train_label)
+
+    print(lr.intercept_, '\n', lr.coef_)
+
+    y1 = lr.predict(test_data)
+    y1 = pd.Series(y1)
+    y1.index = index
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options
+        print(y1)
+    return y1, lr
+
+
+def polynomial_reg_test(train_data, train_label, test_data, index):
+    """
+    多项式回归
+    """
+    quadratic_featurizer = PolynomialFeatures(degree=2)
+    X_train_quadratic = quadratic_featurizer.fit_transform(train_data)
+    regressor_quadratic = LinearRegression(normalize=True)
+    regressor_quadratic.fit(X_train_quadratic, train_label)
+
+    print(regressor_quadratic.intercept_, '\n', regressor_quadratic.coef_)
+
+    test_data = quadratic_featurizer.fit_transform(test_data)
+    y1 = regressor_quadratic.predict(test_data)
+    y1 = pd.Series(y1)
+    y1.index = index
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options
+        print(y1)
+    return y1, regressor_quadratic
 
 
 if __name__ == "__main__":
-    # train data
-    X, Y = convert_data_and_label_to_train(df1, df2a)
-    result_dict = cal_coef(X, Y)
-    for k, v in result_dict.items():
-        print(k, v)
+    # define source file path
+    # source_file = '../queryResult_2019-09-10_145030_processed.xlsx'
+    source_file = '../event_data.npy'
+    df1 = read_source(source_file)
+    # df1.to_pickle('../event_data_simplified.npy')
 
-    a, b = scipy.stats.spearmanr(X, Y)
-    print(a, b)
+    df2 = convert_to_new_dataframe(df1)
+    df2.to_excel('../ndf.xlsx')
+
+    # dm = DataModel(df1)
+    # df1 = dm.cal_all()
+    # df1.to_pickle('../one_line.npy')
+    # df1.to_excel('../one_line.xlsx')
